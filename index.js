841101161f08f987488743e8ad4fa8e077a53415
tmba <ut42.nu@gmail.com>
2025-08-14 00:55:12 +0900

    feat: add pathfinding functionality and update character movement logic

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import pathfinding from "pathfinding";
 import { Server } from "socket.io";
 
 const io = new Server({
@@ -411,8 +412,53 @@ const map = {
   ],
 };
 
+const grid = new pathfinding.Grid(
+  map.size[0] * map.gridDivision,
+  map.size[1] * map.gridDivision
+);
+
+const finder = new pathfinding.AStarFinder({
+  allowDiagonal: true,
+  dontCrossCorners: true,
+});
+
+const findPath = (start, end) => {
+  const gridClone = grid.clone();
+  const path = finder.findPath(start[0], start[1], end[0], end[1], gridClone);
+  return path;
+};
+
+const updateGrid = () => {
+  map.items.forEach((item) => {
+    if (item.walkable || item.wall) {
+      return;
+    }
+    const width =
+      item.rotation === 1 || item.rotation === 3 ? item.size[1] : item.size[0];
+    const height =
+      item.rotation === 1 || item.rotation === 3 ? item.size[0] : item.size[1];
+    for (let x = 0; x < width; x++) {
+      for (let y = 0; y < height; y++) {
+        grid.setWalkableAt(
+          item.gridPosition[0] + x,
+          item.gridPosition[1] + y,
+          false
+        );
+      }
+    }
+  });
+};
+
+updateGrid();
+
 const generateRandomPosition = () => {
-  return [Math.random() * map.size[0], 0, Math.random() * map.size[1]];
+  for (let i = 0; i < 100; i++) {
+    const x = Math.floor(Math.random() * map.size[0] * map.gridDivision);
+    const y = Math.floor(Math.random() * map.size[1] * map.gridDivision);
+    if (grid.isWalkableAt(x, y)) {
+      return [x, y];
+    }
+  }
 };
 
 const generateRandomHexColor = () => {
@@ -439,12 +485,17 @@ io.on("connection", (socket) => {
 
   io.emit("characters", characters);
 
-  socket.on("move", (position) => {
+  socket.on("move", (from, to) => {
     const character = characters.find(
       (character) => character.id === socket.id
     );
-    character.position = position;
-    io.emit("characters", characters);
+    const path = findPath(from, to);
+    if (!path) {
+      return;
+    }
+    character.position = from;
+    character.path = path;
+    io.emit("playerMove", character);
   });
 
   socket.on("disconnect", () => {
